72411e5a7590f793fba10ff3ee3b79d1ad978e97
FelizJueves <jamiedorrens@gmail.com>
2023-02-05 16:21:06 +0000

    Fuck

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer)), RequireComponent(typeof(MeshFilter))]
 public class PlayerController : MonoBehaviour
 {
     public BulletController bullet;
